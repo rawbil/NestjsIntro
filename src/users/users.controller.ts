a75c0f68635b7ae76e825e69b05ebb0a04860e1a
Bil <bildadsimiyu6@gmail.com>
2025-05-14 11:26:29 +0300

    try out GET method in users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  //GET /users
+  @Get()
+  findAll() {
+    return 'All users have been found!!';
+  }
+
+  //GET /users/subscribed
+  @Get('subscribed')
+  findSubs() {
+    return '50 users have subscribed';
+  }
+
+  //GET /users/:id
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return `The userId is: ${id}`;
+  }
+
+  //GET /users/find:rank
+  @Get("find/:rank")
+  findRank(@Param('rank') rank: string) {
+    return `find the user's rank, which in this case is: ${rank}`
+  }
+}
