bc3d100b30889eb1ba619e67009eaaa4fcd5314a
Bil <bildadsimiyu6@gmail.com>
2025-05-22 00:32:42 +0300

    feat: add Prisma module and service for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
