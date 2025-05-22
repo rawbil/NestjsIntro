c1f5ea4fb3151fee9e106d6790cbe31b1ebc14f2
Bil <bildadsimiyu6@gmail.com>
2025-05-22 15:47:15 +0300

    feat: inject jwt service into the auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import {JwtModule} from '@nestjs/jwt';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [PrismaModule]
+  imports: [PrismaModule, JwtModule.register({
+    secret: "7dpQA5XAGR/sNgHxPo9AvSzLrHwwiGb4NgYwMLAp7TE=",
+    signOptions: {expiresIn: '1h'}
+  })]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,19 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
 
-    async validateUser(data: AuthPayloadDto) {
-        const findUser = await this.prisma.users.findUnique({where: {email: data.email as string}});
-        if(!findUser) throw new HttpException("User not found", 404);
-    }
+  async validateUser(data: AuthPayloadDto) {
+    const findUser = await this.prisma.users.findUnique({
+      where: { email: data.email as string },
+    });
+    if (!findUser) throw new HttpException('User not found', 404);
+  }
 }
