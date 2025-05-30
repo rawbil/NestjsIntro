dc41b08be4337e04166c573127cb26c55fdebe0a
hackTivist <bildadsimiyu6@gmail.com>
2025-05-30 21:23:29 +0300

    refactor: remove unused authentication and user modules, controllers, and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [UsersModule, PrismaModule, AuthModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
