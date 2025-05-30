17c163a44ec0ac8dd5d719d55516f5114f7cde9f
hackTivist <bildadsimiyu6@gmail.com>
2025-05-30 22:16:07 +0300

    feat: implement user creation and update DTOs, and enhance UsersService with user retrieval methods

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsStrongPassword()
+    password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
     constructor(private readonly prisma: PrismaService) {}
+
+    //find all users
+    async findAllUsers() {
+        return await this.prisma.users.findMany()
+    }
+
+    //find user by id
+    async findUserById(id: number) {
+        const user = await this.prisma.users.findUnique({where: {id}});
+        if(!user) throw new NotFoundException("User not found");
+        return user;
+    }
+
+    //create new user
+    async createUser(data: Prisma.UsersCreateInput) {
+        
+    }
+
 }
