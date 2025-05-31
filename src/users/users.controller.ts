e89802024224ea568e870ed3e0a99f84c50a5bc8
hackTivist <bildadsimiyu6@gmail.com>
2025-05-31 08:44:03 +0300

    feat: add CRUD methods in users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, ValidationPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get('find-all') //GET /users/find-all
+    findAllUsers() {
+        return this.usersService.findAllUsers()
+    }
+
+    @Get(':id') //GET /users/:id
+    findUserById(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.findUserById(id);
+    }
+
+    @Post('create-user') //POST /users/create-user
+    createUser(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+        return this.usersService.createUser(createUserDto);
+    }
+
+    @Patch('update/:id') //PATCH /users/update/:id
+    updateUserById(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUserDto) {
+        return this.usersService.updateUserById(id, updateUserDto);
+    }
+
+    @Delete('delete/:id') //DELETE /users/delete/:id
+    deleteUser(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.deleteUser(id);
+    }
+}
