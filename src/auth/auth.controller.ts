27ddf038dba237d21486e1b9663e5996ee11c8d9
hackTivist <bildadsimiyu6@gmail.com>
2025-06-01 18:51:59 +0300

    feat: add JwtAuthGuard to auth and users controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth-guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {} //inject AuthService
 
     @Post('register') //POST /auth/register
+    //@UseGuards(JwtAuthGuard)
     register(@Body() registerDto: RegisterDto) {
         return this.authService.register(registerDto);
     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Get('find-all') //GET /users/find-all
     findAllUsers() {
         return this.usersService.findAllUsers()
